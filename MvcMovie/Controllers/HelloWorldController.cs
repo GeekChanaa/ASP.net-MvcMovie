b014e28de0a05f1671cd5c2e1e09d2b2a02e2219
chanaa <mohammedchanaa@hotmail.com>
2020-12-17 14:31:35 +0100

    initial

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string name, int level = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.Level = level;
+            return View();
+        }
+
+    }
+}
